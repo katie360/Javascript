4ceef91d6a7fc483abcf011f0e5c73771d80191a
katie360 <kateratemo44@gmail.com>
2022-10-02 18:27:39 +0300

    number methods

--- index.js
+++ index.js
@@ -32,3 +32,11 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const deleteButton = document.querySelector('[data-delete]')
 const firstOperandTextElement = document.querySelector('[data-first-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+
+// number methods
+let PI = 3.14159;
+let num = PI.toFixed(3);
+console.log(num);
+let num2 = PI.toPrecision(3);
+console.log(num2);
